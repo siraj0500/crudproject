4a8fde0b70d2c4f5ad02f6b2b8e87e8c68b62ef5
sirajudheen <sirajudheen1ta@gmail.com>
2022-04-11 22:11:33 +0530

    Initial Commit

--- crudproject1/settings.py
+++ crudproject1/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$^*-#$qc0jy)l=dh*2k*4*o0hyq%#^70&-xrn^*s&00%z6h$%'
+from decouple import config
+
+SECRET_KEY = config("django-insecure-4$^*-#$qc0jy)l=dh*2k*4*o0hyq%#^70&-xrn^*s&00%z6h$%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
